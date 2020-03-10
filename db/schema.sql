3c30fe91bf0e35e900a4e91bd37fdecfcf5ff22b
Dawson Hensel <dawsonhensel@gmail.com>
2020-03-10 16:20:39 -0700

    finished schema and seeds db files

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers(
+    id int primary key auto_increment,
+    burger_name varchar(50) not null,
+    devoured boolean
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+insert into burgers (burger_name, devoured)
+values ('BBQ Burger', false), ('Veggie Burger', false), ('Vegan Burger', false);
