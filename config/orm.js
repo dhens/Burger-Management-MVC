1d94d4d9696b1713d9297032a30fb1ea57f1d520
Dawson Hensel <dawsonhensel@gmail.com>
2020-03-10 18:15:08 -0700

    wrote inital orm printQuestionMarks function

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,20 @@
+// import mysql connection
+const connection = require('../config/connection');
 
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+const printQuestionMarks = (num) => {
+    const arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push('?');
+    }
+
+    // Return s a string representtion of the array - comma separated
+    return arr.toString();
+}
 
 module.exports = orm;
