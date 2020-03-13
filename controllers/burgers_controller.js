1d5a1c4d117ac9032f12e2a2bd0a8d8617ebc0ab
Dawson Hensel <dawsonhensel@gmail.com>
2020-03-12 18:31:23 -0700

    changed orm file to model the Cats activity

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions
-const burger = require('../models/burger');
+const db = require('../models');
 
 // Create all our routes and set up logic within those routes where required
 router.get('/', (req, res) => {
