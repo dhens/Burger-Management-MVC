5ff93bb34f1d82a43233b44f9262267c6fcf5c55
Dawson Hensel <dawsonhensel@gmail.com>
2020-03-12 19:30:34 -0700

    render current burgers on front page completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions
-const db = require('../models');
+const burger = require('../models/burger');
 
 // Create all our routes and set up logic within those routes where required
 router.get('/', (req, res) => {
@@ -13,4 +13,9 @@ router.get('/', (req, res) => {
         console.log(hbsObject);
         res.render('index', hbsObject)
     })
-})
+});
+
+// router.post
+
+// Export routes for server.js to read and use
+module.exports = router;
